d9be82e9079dcdb66e953fca8968dde6e3dfc8c2
Maryame <elhilali.maryame2@gmail.com>
2023-06-15 16:44:24 -0700

    Printf Function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ * C program that uses the printf function to print in stdout
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
