faefee0a2c6cb0ef3430fa99e7644eacbed84014
Maryame <elhilali.maryame2@gmail.com>
2023-06-16 16:43:11 -0700

    Print Numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program to print numbers for 0 to 9
+ *
+ * Return: Always 0 that means success
+ */
+
+int main(void)
+
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar (a);
+	}
+	putchar ('\n');
+
+	return (0);
+}
