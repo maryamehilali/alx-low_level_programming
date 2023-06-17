d084b4074e81378082011f1749f0d5b967e71d9f
Maryame <elhilali.maryame2@gmail.com>
2023-06-16 18:29:49 -0700

    Print Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int a;
 
-	putchar('0');
-
-	for (a = 1; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
+		putchar((a % 10) + '0');
+		if (a == 9)
+			continue;
 		putchar (',');
 		putchar (' ');
-		putchar ((a % 10) + '0');
 	}
 
 	putchar ('\n');
