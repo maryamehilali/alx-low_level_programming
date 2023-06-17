e0f454aaee250dfe50a5dc4b4b6e444358a7b6a5
Maryame <elhilali.maryame2@gmail.com>
2023-06-16 18:14:05 -0700

    Print Comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - program to print numbers separated by comma and a space.
+ *
+ * Return: Always 0 that means success
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar ((a % 10) + '0');
+		putchar (',');
+		putchar (' ');
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
