680b10548b94c311b0358e800dc3b79368c62c2e
Maryame <elhilali.maryame2@gmail.com>
2023-06-17 18:07:07 -0700

    Print comb 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all possible combinaition of two digits;
+ * digits should be different;
+ * only print the smallets combinaition;
+ * combinations should be saparated by a comma and a space;
+ * a new line at the end;
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+	for (m = 0; m <= 9; m++)
+	{
+	if (m != n && m > n)
+	{
+		putchar((n % 10) + '0');
+		putchar((m % 10) + '0');
+		if (m != 9 || n != 8)
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
