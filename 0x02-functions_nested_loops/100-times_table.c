fe777b6a14c6a7e3942d87da3ba0f8d1e05d3c86
Maryame <elhilali.maryame2@gmail.com>
2023-06-21 16:12:36 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,47 +9,34 @@
 void print_times_table(int n)
 {
 	if (n < 15 && n > 0)
-	{
-	int i = 0;
+	{	int i = 0;
 
 	while (i <= n)
-	{
-		int m = 0;
+	{	int m = 0;
 
 		while (m <= n)
-		{
-		int k = i * m;
+		{int k = i * m;
 
 		if (k == 0 && m == 0)
 		_putchar('0');
 		else if (k >= 100)
-		{
-		_putchar (',');
+		{_putchar (',');
 		_putchar (' ');
 		_putchar (k / 100 + '0');
 		_putchar ((k / 10) % 10 + '0');
-		_putchar (k % 10 + '0');
-		}
+		_putchar (k % 10 + '0'); }
 		else if (k >= 10)
-		{
-		_putchar (',');
+		{_putchar (',');
 		_putchar (' ');
 		_putchar (' ');
 		_putchar (k / 10 + '0');
-		_putchar (k % 10 + '0');
-		}
+		_putchar (k % 10 + '0'); }
 		else
-		{
-		_putchar (',');
+		{_putchar (',');
 		_putchar (' ');
 		_putchar (' ');
 		_putchar (' ');
-		_putchar(k + '0');
-		}
-		m++;
-		}
+		_putchar(k + '0'); }
+		m++; }
 	i++;
-	_putchar('\n');
-	}
-	}
-}
+	_putchar ('\n');	}}}
