bee584994fa994a9bc411901977f5e59751a4dbd
Maryame <elhilali.maryame2@gmail.com>
2023-06-20 19:06:07 -0700

    Add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that gives the sum of é numbers.
+ * @a: first number.
+ * @b: second number
+ * Return: the sum.
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
