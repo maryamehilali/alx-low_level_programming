d4b5bf994f7c3cd9c1e9d7eaa2dc2c4917e96166
Maryame <elhilali.maryame2@gmail.com>
2023-06-23 16:06:32 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints umbers from 0 to 9.
+ *
+ * Return: 0 always success.
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for(c = 0; c <= 9; c++)
+		_putchar(c + '0');
+}
