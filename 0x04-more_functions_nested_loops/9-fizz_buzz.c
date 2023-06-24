b731954e78742578ba5b33257a99acd00b0a0784
Maryame <elhilali.maryame2@gmail.com>
2023-06-23 18:00:29 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -21,6 +21,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
+	printf("Buzz");
 	printf("\n");
 	return (0);
 }
