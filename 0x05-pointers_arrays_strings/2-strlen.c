8f41a915a3de8185887cb71e9e986c2579ed3d33
Maryame <elhilali.maryame2@gmail.com>
2023-06-26 19:10:56 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - function to determene the lenght of a string.
+ * @s: string to determne it s length.
+ * Return: Length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int i, length;
+
+	length = 0;
+
+	for (i = 0; i < 100; i++)
+	{
+		if (*(s + i) != '\0')
+			length++;
+		else
+			break;
+	}
+	return (length);
+}
