89eecc9f777126a5fa06411824b2fee8283acfc9
Maryame <elhilali.maryame2@gmail.com>
2023-07-03 17:42:25 -0700

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints the second half of a string
+ * @str: the input string
+ */
+void puts_half(char *str)
+{
+	int size, i, n;
+
+	size = 0;
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		size = i;
+		i++;
+	}
+
+	if (size % 2 == 0)
+		n = size / 2;
+	else
+		n = (size + 1) / 2;
+
+	for (i = n; i <= size ; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
