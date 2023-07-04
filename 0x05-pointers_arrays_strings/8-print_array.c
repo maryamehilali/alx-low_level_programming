5306ac6750e9d71f1d0fd9404c749039623f32b4
Maryame <elhilali.maryame2@gmail.com>
2023-07-03 18:33:41 -0700

    n elements of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,8 +14,10 @@ void print_array(int *a, int n)
 	if (n > 0)
 	{
 	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
+	{	printf("%d, ", a[i]);
+		printf("%d\n", a[i]);
 	}
-
-	printf("%d\n", a[i]);
+	}
+	else
+		printf("\n");
 }
