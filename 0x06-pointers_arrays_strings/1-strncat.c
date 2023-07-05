2d214eff39d3bd27f27b392fed890b94b1adedeb
Maryame <elhilali.maryame2@gmail.com>
2023-07-04 18:18:42 -0700

    string concatenate  n bytes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,34 +9,24 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int size1, size2, i, j;
+	int i, j;
 
 	i = 0;
 	j = 0;
-
+	
 	while (dest[i] != '\0')
 	{
-		size1 = i;
 		i++;
 	}
 
-	while (src[j] != '\0')
+	while (src[j] != '\0' && j < n)
 	{
-		size2 = j;
+		dest[i] = src[j];
 		j++;
+		i++;
 	}
 
-	if (size1 == 0)
-		size1 = -1;
-
-	if (n > size2)
-		n = size2;
-
-	for (i = 0; i < n; i++)
-	{
-		dest[size1 + 1 + i] = src[i];
-	}
-	dest[size1 + n + 1] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
