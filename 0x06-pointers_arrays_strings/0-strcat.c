a2495d7f1eab0122153d8126721b88f931108c3d
Maryame <elhilali.maryame2@gmail.com>
2023-07-04 18:30:36 -0700

    add two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,30 +8,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int size1, size2, i, j;
+	int i, j;
 
 	i = 0;
 	j = 0;
 
 	while (dest[i] != '\0')
-	{
-		size1 = i;
 		i++;
-	}
 
 	while (src[j] != '\0')
 	{
-		size2 = j;
+		dest[i] = src[j];
 		j++;
+		i++;
 	}
-	if (size1 == 0)
-		size1 = -1;
-	{
-	for (i = 0; i <= size2; i++)
-	{
-		dest[size1 + 1 + i] = src[i];
-	}
-	dest[size1 + size2 + 2] = '\0';
-	}
+	dest[i] = '\0';
 	return (dest);
 }
