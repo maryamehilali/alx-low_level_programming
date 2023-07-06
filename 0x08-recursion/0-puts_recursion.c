bb7fc685a6d5e9a07fae2c726250f3eea0bc7b30
Maryame <elhilali.maryame2@gmail.com>
2023-07-05 18:43:34 -0700

    print string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,14 +9,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (s[0] == 0)
-	{
-		_putchar('\0');
-		_putchar('\n');
-	}
-	else
+	if (s[0] != 0)
 	{
 		_putchar(s[0]);
 		_puts_recursion(s + 1);
 	}
+	else
+		_putchar('\n');
 }
