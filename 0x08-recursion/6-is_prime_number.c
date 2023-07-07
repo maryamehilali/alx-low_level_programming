388b3e74a2c493659c24d13b440046e99f499dd8
Maryame <elhilali.maryame2@gmail.com>
2023-07-06 19:16:47 -0700

    prinme Numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_prime_number - that returns 1 if the input integer is a prime number,
+ * otherwise return 0.
+ * @n: input number
+ * Return: 0 or 1.
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else
+		return (prime_search(n, n - 1));
+}
+/**
+ * prime_search - that verify if the input integer is divisible by i,
+ * while i different then 1.
+ * @n: input number
+ * @i: is n can be divided by i 
+ * Return: 0 or 1.
+ */
+
+int prime_search(int n, int i)
+{
+	if (i < 1)
+		return (0);
+	else if (n % i == 0 && i != 1)
+		return (0);
+	else if (n % i == 0 && i == 1)
+		return (1);
+	else
+		return (prime_search(n, i - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int square_search(int n, int i);
 int is_prime_number(int n);
+int prime_search(int n, int i);
 
 #endif /* MAIN_H */
