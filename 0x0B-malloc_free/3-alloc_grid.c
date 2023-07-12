d4a7cf0ec05b3b785c8076995aea61e04ca0aeca
Maryame <elhilali.maryame2@gmail.com>
2023-07-11 19:45:08 -0700

    allocate a two dimension array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,8 +25,9 @@ int **alloc_grid(int width, int height)
 		ptr[i] = malloc(sizeof(int) * width);
 	if (ptr[i] == NULL)
 	{
+		for (; i >= 0; i--)	
 		free(ptr[i]);
-		free(ptr);
+	free(ptr);
 		return (NULL);
 	}
 	}
