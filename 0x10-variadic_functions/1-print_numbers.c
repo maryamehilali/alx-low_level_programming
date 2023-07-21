9fd598923c7268cbbd3f4d722f9b7ab1d75b4cda
Maryame <elhilali.maryame2@gmail.com>
2023-07-20 19:31:38 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(print_num, int));
 		if (separator != NULL && i < n - 1)
-			printf("%s ", separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(print_num);
