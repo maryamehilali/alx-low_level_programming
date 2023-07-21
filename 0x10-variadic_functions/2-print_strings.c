7a477d74ded38a17aba8ada703559ad81e25dd72
Maryame <elhilali.maryame2@gmail.com>
2023-07-20 19:47:06 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -28,7 +28,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (!str)
 			printf("(nil)");
 		printf("%s", str);
-		if (separator != NULL && i < n - 1)
+		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	}
 	va_end(print_str);
