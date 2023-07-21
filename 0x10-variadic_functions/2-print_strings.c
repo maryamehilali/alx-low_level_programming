591babd77f51696182da9126d6503b8a5c2b2c9f
Maryame <elhilali.maryame2@gmail.com>
2023-07-20 19:21:10 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,7 @@
-#include <stdlib.h>
-#include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include "variadic_functions.h"
 
 /**
  * print_strings - prints string
