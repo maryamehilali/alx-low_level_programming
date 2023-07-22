6a699044a1ecc477bdc8210d648e7a1a2088f63e
Maryame <elhilali.maryame2@gmail.com>
2023-07-21 19:02:43 -0700

    int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,12 +14,13 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int i;
 
 	if (array && cmp)
 	{
 		if (size <= 0)
 			return (-1);
+	
 		for (i = 0, i < size, i++)
 		{
 			if (cmp(array[i]))
