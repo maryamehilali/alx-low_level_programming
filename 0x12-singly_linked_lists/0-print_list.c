44700a37b3dc99929b6c2aea8ab75da14b4e7043
Maryame <elhilali.maryame2@gmail.com>
2023-07-27 17:38:49 -0700

    Singly Linked Lists, print all elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints all elements of a list.
+ * @h: the linked list to print.
+ * Return: number of nodes on the list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr;
+
+	ptr = h;
+
+	if (ptr == NULL)
+		return (-1);
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
