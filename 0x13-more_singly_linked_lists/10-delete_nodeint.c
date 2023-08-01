0a2dfc1007adf3dd05c7ab4bbeb5c5c08c442848
Maryame <elhilali.maryame2@gmail.com>
2023-07-31 18:49:43 -0700

    delete a node at an index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,22 +16,22 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *ptr, *temp;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 	ptr = *head;
-	if (idx == 0)
+	if (index == 0)
 	{
 		ptr = ptr->next;
 		free(*head);
 		*head = ptr;
 		return (1);
 	}
-	while (ptr != NULL && i < (idx - 1))
+	while (ptr != NULL && i < (index - 1))
 	{
 		ptr = ptr->next;
 		i++;
 	}
-	if (i == (idx - 1))
+	if (i == (index - 1))
 	{
 		temp = ptr->next;
 		ptr->next = temp->next;
