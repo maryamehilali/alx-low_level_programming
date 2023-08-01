001f00b17b9d579571412972fbcab5f72860ee44
Maryame <elhilali.maryame2@gmail.com>
2023-07-31 18:15:19 -0700

    insert a node at index case head is NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 	ptr = *head;
 	if (idx == 0)
