774237d5c00200e294e7db13dbba831530eaf680
Maryame <elhilali.maryame2@gmail.com>
2023-08-03 19:39:10 -0700

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * recursive_binary - function to print binary by recursion
+ * @n: int to print.
+ * Return: void.
+ */
+
+void recursive_binary(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	recursive_binary(n >> 1);
+	_putchar((n & 1)+'0');
+}
+
+/**
+ * print_binary - function that prints the binary representation of an int
+ * @n: the input unsigned long int.
+ * Return: void.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar ('0');
+	}
+	else
+		recursive_binary(n);
+}
