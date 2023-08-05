db11384873158100063baa388f34a626069aa1ba
Maryame <elhilali.maryame2@gmail.com>
2023-08-04 19:48:44 -0700

    get endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - function that checks endianess of a machine.
+ * Return: 0 if big endian, 1 if little endian.
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+	return (0);
+}
