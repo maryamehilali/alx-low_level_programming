1451fe5fd8b6c523722bdbc4050b2c1cceec4aae
Maryame <elhilali.maryame2@gmail.com>
2023-08-08 16:57:08 -0700

    copy a file missing checks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,20 +22,22 @@ int main(int argc, char **argv)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97); }
 	source_file = open(argv[1], O_RDONLY);
-	n_read = read(source_file, buffer, BUFFER_SIZE);
-	if (n_read == -1 || n_read == 0 || source_file == -1)
+	if (source_file == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
 	dest_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	n_read = BUFFER_SIZE;
 	while (n_read == BUFFER_SIZE)
 	{
+		n_read = read(source_file, buffer, BUFFER_SIZE);
+		if (n_read == -1)
+		{ dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98); }
 		n_write = write(dest_file, buffer, BUFFER_SIZE);
 		if (n_write == -1 || dest_file == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		{ dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			exit(99); }
-		n_read = read(source_file, buffer, BUFFER_SIZE);
 	}
 	n_write = write(dest_file, buffer, n_read);
 	n_close1 = close(source_file);
