be36d41ea558157100540d2a8b7f898e1977ba31
Maryame <elhilali.maryame2@gmail.com>
2023-08-07 18:26:45 -0700

    task 1 check 8

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,8 +19,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	len = strlen(text_content);
-	n_byte = write(fd, text_content, len);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+		n_byte = write(fd, text_content, len);
+	}
 	if (n_byte == -1)
 		return (-1);
 	close(fd);
