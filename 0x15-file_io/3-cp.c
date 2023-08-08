18e87e56d59a40943bd902a6bca3ffbf947db54e
Maryame <elhilali.maryame2@gmail.com>
2023-08-08 16:49:46 -0700

    copy a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv)
 		exit(97); }
 	source_file = open(argv[1], O_RDONLY);
 	n_read = read(source_file, buffer, BUFFER_SIZE);
-	if (n_read == -1 || source_file == -1)
+	if (n_read == -1 || n_read == 0 || source_file == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
