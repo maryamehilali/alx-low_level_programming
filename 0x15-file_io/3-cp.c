451728af25a72ca64588ad3d7a3a448d38833885
Maryame <elhilali.maryame2@gmail.com>
2023-08-08 16:01:36 -0700

    copy a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,21 +14,19 @@
 int main(int argc, char **argv)
 {
 	int n_read, n_write, source_file, dest_file;
+	int n_close1, nclose2;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
+		exit(97); }
 	source_file = open(argv[1], O_RDONLY);
 	n_read = read(source_file, buffer, BUFFER_SIZE);
-	if (n_read == -1 || source_file == -1)
+	if (n_read == -1 || source_file == -1 || source_file == NULL)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		exit(98); }
 	dest_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while (n_read == BUFFER_SIZE)
 	{
@@ -36,12 +34,21 @@ int main(int argc, char **argv)
 		if (n_write == -1 || dest_file == -1)
 		{
 			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
+			exit(99); }
 		n_read = read(source_file, buffer, BUFFER_SIZE);
 	}
 	n_write = write(dest_file, buffer, n_read);
-	close(source_file);
-	close(dest_file);
+	n_close1 = close(source_file);
+	n_close2 = close(dest_file);
+	if (n_close1 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", source_file);
+		exit(100);
+	}
+	if (n_close2 == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", dest_file);
+		exit(100);
+	}
 	return (0);
 }
