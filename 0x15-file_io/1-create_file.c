1799b316118d4bfa3980b8c2e8f691822154ab61
Maryame <elhilali.maryame2@gmail.com>
2023-08-07 18:05:25 -0700

    creat a file chack 8

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	int len;
 
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
