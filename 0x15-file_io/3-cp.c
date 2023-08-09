8e149e8913678d15871ca5b182452ee559bfe5a9
Maryame <elhilali.maryame2@gmail.com>
2023-08-08 18:54:17 -0700

    check 15 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,10 +25,10 @@ int main(int argc, char **argv)
 	if (n_read == -1 || source_file == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	dest_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 664);
+	dest_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	while (n_read == BUFFER_SIZE)
 	{
-		n_write = write(dest_file, buffer, BUFFER_SIZE);
+		n_write = write(dest_file, buffer, n_read);
 		if (n_write == -1 || dest_file == -1)
 		{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99); }
@@ -38,7 +38,7 @@ int main(int argc, char **argv)
 			exit(98); }
 	}
 	n_write = write(dest_file, buffer, n_read);
-	if (n_write == -1 || dest_file == -1)
+	if (n_write == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99); }
 	n_close1 = close(source_file);
