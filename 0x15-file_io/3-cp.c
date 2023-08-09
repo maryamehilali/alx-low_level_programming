5a0ba9f94cf548791a5423c07559fd3f1c1f5ee8
Maryame <elhilali.maryame2@gmail.com>
2023-08-08 17:19:51 -0700

    copy a file missing checks

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,7 +35,13 @@ int main(int argc, char **argv)
 			exit(99); }
 		n_read = read(source_file, buffer, BUFFER_SIZE);
 	}
+	if (n_read == -1 || source_file == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
 	n_write = write(dest_file, buffer, BUFFER_SIZE);
+	if (n_write == -1 || dest_file == -1)
+	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99); }
 	n_close1 = close(source_file);
 	n_close2 = close(dest_file);
 	if (n_close1 == -1)
