d3f727f89a0c10fdc5da5ee9c73a1d1c3f124778
Maryame <elhilali.maryame2@gmail.com>
2023-08-08 18:26:04 -0700

    copy a file missing check 15

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,22 +25,19 @@ int main(int argc, char **argv)
 	if (n_read == -1 || source_file == -1)
 	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98); }
-	dest_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 00664);
+	dest_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC , 0664);
 	while (n_read == BUFFER_SIZE)
 	{
-		if (n_read == -1 || source_file == -1)
-		{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98); }
 		n_write = write(dest_file, buffer, BUFFER_SIZE);
 		if (n_write == -1 || dest_file == -1)
 		{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99); }
 		n_read = read(source_file, buffer, BUFFER_SIZE);
+		if (n_read == -1 || source_file == -1)
+		{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98); }
 	}
 	n_write = write(dest_file, buffer, BUFFER_SIZE);
-	if (n_write == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99); }
 	n_close1 = close(source_file);
 	n_close2 = close(dest_file);
 	if (n_close1 == -1)
