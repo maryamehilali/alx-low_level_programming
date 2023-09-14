b40a93a7bd93e50434cd27af4cf905786f1564d0
Maryame <elhilali.maryame2@gmail.com>
2023-09-14 16:12:46 -0700

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - function that prints the ellements of doubly linked list
+ * @h: pointer to the head of the list
+ * Return: number of nodes in the list
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	size_t count = 0;
+
+	ptr = h;
+	while (ptr)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
