69eab3462f6ca5040d9fd534a1a200ebdc8c3e4f
Maryame <elhilali.maryame2@gmail.com>
2023-10-06 15:23:19 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,15 +8,18 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *tmp;
 
 	if (ht == NULL || ht->size == 0 || key == NULL)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
+	tmp = ht->array[index];
+	while (tmp)
 	{
-		return (ht->array[index]->value);
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
 	}
 	return (NULL);
 }
