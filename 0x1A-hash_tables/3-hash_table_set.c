597eef476d87d9a3e1c4b5fb05928826c1365a1c
Maryame <elhilali.maryame2@gmail.com>
2023-10-06 15:06:06 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,13 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-	element = (hash_node_t *) malloc(sizeof(hash_node_t));
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = element;
-		return (1);
-	}
-	else if (strcmp(ht->array[index]->key, key) == 0)
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 	{
 		ht->array[index]->value = strdup(value);
 		return (1);
@@ -37,6 +31,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		element->value = strdup(value);
 		element->next = ht->array[index];
 		ht->array[index] = element;
-		return (1);
 	}
+	return (1);
 }
