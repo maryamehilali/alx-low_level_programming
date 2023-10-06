a1927f580769751da65aadbe00f1786046fccbb7
Maryame <elhilali.maryame2@gmail.com>
2023-10-06 07:58:56 -0700

    trying others code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,36 +1,39 @@
 #include "hash_tables.h"
-#include <string.h>
+
 /**
  * hash_table_set - adds an element to the hash table
- * @key: the key of the element
- * @value: value of the element
- * Return: 1 on success 0 on fail.
- */
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key
+ * Return: 1 if its succeeded 0 otherwise
+**/
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *element = NULL;
-	unsigned long int index;
+	unsigned long d, size;
+	hash_node_t *n_node;
 
-	if (ht == NULL || ht->size == 0 || key == NULL || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	element = (hash_node_t *) malloc(sizeof(hash_node_t));
-	if (element == NULL)
-		return (0);
-	element->key = strdup(key);
-	if (value != NULL)
-		element->value = strdup(value);
-	element->next = NULL;
-	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index] == NULL)
+
+	size = ht->size;
+	d = key_index((const unsigned char *)key, size);
+
+	if (ht->array[d] != NULL && strcmp(ht->array[d]->key, key) == 0)
 	{
-		ht->array[index] = element;
+		ht->array[d]->value = strdup(value);
 		return (1);
 	}
-	else
-	{
-		ht->array[index]->next = element;
-		return (1);
-	}
-	return (0);
 
+	n_node = malloc(sizeof(hash_node_t));
+
+	if (n_node == NULL)
+		return (0);
+
+	n_node->key = strdup(key);
+	n_node->value = strdup(value);
+	n_node->next = ht->array[d];
+	ht->array[d] = n_node;
+
+	return (1);
 }
